f41bdee07b7c35a457d4ca81b55038b6183fe31f
hirasawaa <ahikoki77@gmail.com>
2022-06-14 21:33:56 +0900

    フォームリクエスト

--- app/Http/Controllers/Owner/shopController.php
+++ app/Http/Controllers/Owner/shopController.php
@@ -8,6 +8,7 @@ use App\Models\Shop;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 use InterventionImage;
+use App\Http\Requests\UploadImageRequest;
 
 class shopController extends Controller
 {
@@ -46,7 +47,7 @@ class shopController extends Controller
         return view('owner.shops.edit',compact('shop'));
     }
 
-    public function update(Request $request, $id)
+    public function update(UploadImageRequest $request, $id)
     {
         $imageFile = $request->image;
         if(!is_null($imageFile) && $imageFile->isValid()){
