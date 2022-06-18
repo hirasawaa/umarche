aa86a2ab007a0ddb074a14282b152f78535d64aa
hirasawaa <ahikoki77@gmail.com>
2022-06-18 21:22:26 +0900

    Product初期設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,9 @@ class DatabaseSeeder extends Seeder
             OwnerSeeder::class,
             ShopSeeder::class,
             ImageSeeder::class,
+            CategorySeeder::class,
+            ProductSeeder::class,
+
         ]);
 
     }
